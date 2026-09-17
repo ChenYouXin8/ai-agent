7a30d221fd6edc8f6d3e26eb0941add671e19cc6
Li <154851375+ChenYouXin8@users.noreply.github.com>
2026-09-17 13:41:47 +0800

    feat: make task steps persistable

--- src/main/java/io/github/chenyouxin8/chenaiagent/task/TaskStep.java
+++ src/main/java/io/github/chenyouxin8/chenaiagent/task/TaskStep.java
@@ -2,8 +2,12 @@ package io.github.chenyouxin8.chenaiagent.task;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class TaskStep {
+public class TaskStep implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final String stepId;
     private final int sequence;
     private final String title;
